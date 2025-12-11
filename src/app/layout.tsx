4ad010313f0ca6790e6e4c9787145cd901593316
Reinaldo-GK <arifin.reinaldo@gmail.com>
2025-12-11 15:44:53 +0800

    commit icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,19 @@ import RouteLoading from "./RouteLoading";
 export const metadata: Metadata = {
   title: `${process.env.NEXT_PUBLIC_APP_NAME || "Pet Manager"} - Admin Panel`,
   description: `Manage your pets with ${process.env.NEXT_PUBLIC_APP_NAME || "Mine17"}`,
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        sizes: 'any',
+      },
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
